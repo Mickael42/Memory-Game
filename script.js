b78c6f934ea6ade1b8dbdc4f066628fbbe58455a
Mickael42 <48717155+Mickael42@users.noreply.github.com>
2019-05-28 17:19:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ class Card {
 
 
     constructor() {
-        this.unicodes = ['🐓', '🍾', '🇫🇷', '🚒', '🐓', '🍾', '🇫🇷','🚒'];
+        this.unicodes = ['🐓', '🍾', '🇫🇷', '🍺', '🚒', '🐓', '🍾', '🐄', '🇫🇷', '🚒', '🍺', '🐄'];
         this.shuffle(this.unicodes);
         this.card1;
         this.card2;
         this.numberOfClick = 0;
         this.idSelectedCard;
         this.signCard;
+        this.cardPlace = document.querySelector('.row');
+        this.classCard;
 
 
         for (let i = 0; i < this.unicodes.length; i++) {
-
             this.creatCard(i, this.unicodes[i]);
         }
     };
 
     creatCard(i, unicodes) {
-        this.cardContainer = document.write('<div class="cardCont" id ="card' + i + '" name="' + unicodes + '">' + unicodes + '</div>');
+        this.cardPlace.innerHTML += '<div class="col-lg-3 colCard" name="rowCard' + unicodes + '" ><div class="cardCont" id ="card' + i + '" name="' + unicodes + '">' + unicodes + '</div></div>';
     };
 
     shuffle(array) {
@@ -42,39 +43,40 @@ class Card {
         return array;
     };
 
-    play() {
-        this.cardsSelected = document.querySelectorAll('.cardCont');
-        this.cardsSelected.forEach(cardSelected => {
-            cardSelected.addEventListener('click', event => {
+play() {
+    this.cardsSelected = document.querySelectorAll('.cardCont');
 
-                if (this.idSelectedCard !== event.target.id) {
-                    this.returnCard(cardSelected);
-                    console.log(event.target.innerHTML);
+    this.cardsSelected.forEach(cardSelected => {
+        cardSelected.addEventListener('click', event => {
+            if (this.idSelectedCard !== event.target.id && event.target.className !== "cardSuccess" && this.numberOfClick < 2) {
+                this.returnCard(cardSelected);
 
-                    if (this.signCard == event.target.innerHTML) {
-                        let cardsWinner = document.getElementsByName(this.signCard);
-                        this.ifWin(cardsWinner);
-                    }
+            }
+            if (this.card1 == this.card2) {
+                this.cardsWinner = document.getElementsByName(this.card1);
+                this.cardsWinner.forEach(cardWinner => {
+                    this.ifWin(cardWinner);
+                });
 
-                    this.idSelectedCard = event.target.id;
-                    this.signCard = event.target.innerHTML;
-                    this.numberOfClick++;
-                    
-                    setTimeout(() => {
-                        this.ifWrong(cardSelected);
-                    }, 1000);
+            }
+            this.idSelectedCard = event.target.id;
+            this.signCard = event.target.innerHTML;
 
-                }
-            });
-        });
-
-    }
+            if (event.target.className !== "cardSuccess") {
+                setTimeout(() => {
+                    this.ifWrong(cardSelected);
+                }, 1500);
+            }
+        }
+        );
+    });
+}
 
     returnCard(cardSelected) {
 
-        if (this.numberOfClick <= 1) {
+        if (this.numberOfClick < 1) {
             this.selectCard1(cardSelected);
-        } else {
+        } else if (this.numberOfClick = 1) {
             this.selectCard2(cardSelected);
         }
 
@@ -87,29 +89,51 @@ class Card {
 
     };
 
-
     selectCard2(cardSelected) {
         //selection carte 2
         cardSelected.className = 'cardBack';
         this.card2 = event.target.innerHTML;
+        this.numberOfClick = 2;
+    }
+
+
+    ifWin(cardWinner) {
+        cardWinner.className = "cardSuccess";
         this.numberOfClick = 0;
-    }
+        this.card1 = "";
+        this.card2 = "";
+        /* cardWinner.style.backgroundColor = "green";  */
+    };
 
-
-    ifWin(cardsWinner) {
-        cardsWinner.forEach(cardWinner => {
-            cardWinner.style.backgroundColor = "green";
-        });
-
-    }
 
     ifWrong(cardSelected){
-        cardSelected.className='cardCont';
+        cardSelected.className = 'cardCont';
+        this.numberOfClick = 0;
+        this.card1 = "";
+        this.card2 = "";
+    }
+
+    reloadGame(){
+        let refresh = document.querySelector('button');
+        refresh.addEventListener('click', location.reload(), false)
+    };
+
+
+    restart(){
+        this.button = document.querySelector('button');
+        this.button.addEventListener('click', event => {
+            this.reloadGame();
+        })
     }
 }
 
+
+
+
+
 let game = new Card();
 game.play()
+game.restart();
 
 
 
