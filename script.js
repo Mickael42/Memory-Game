e732d6e66b335cf739f48177b9b6e03b87f4eb14
Mickael42 <48717155+Mickael42@users.noreply.github.com>
2019-06-02 11:33:13 +0200

    VF

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ class Card {
 
 
     constructor() {
-        this.unicodes = ['🐓', '🍾', '🇫🇷', '🍺', '🚒', '🐓', '🍾', '🐄', '🇫🇷', '🚒', '🍺', '🐄'];
+        this.unicodes = ['🐓', '🍾', '🌄', '🍺', '🚒', '🐓', '🍾', '🐄', '🌄', '🚒', '🍺', '🐄'];
         this.shuffle(this.unicodes);
-        this.card1;
-        this.card2;
+        this.card1Symbol;
+        this.card2Symbol;
         this.numberOfClick = 0;
-        this.idSelectedCard;
-        this.signCard;
         this.cardPlace = document.querySelector('.row');
         this.classCard;
+        this.numberOfCorrect=0;
+        this.numberOfTotalClick = 0;
+        this.card1Id;
 
 
         for (let i = 0; i < this.unicodes.length; i++) {
@@ -20,7 +21,13 @@ class Card {
     };
 
     creatCard(i, unicodes) {
-        this.cardPlace.innerHTML += '<div class="col-lg-3 colCard" name="rowCard' + unicodes + '" ><div class="cardCont" id ="card' + i + '" name="' + unicodes + '">' + unicodes + '</div></div>';
+        let divCard = document.createElement("div");
+        divCard.setAttribute('class', 'col-md-3 colCard');
+        divCard.setAttribute('data-symbol', ''+unicodes+'')
+        divCard.setAttribute('id', 'card'+i+'');
+        let symbolCard = document.createTextNode(unicodes);
+        divCard.appendChild(symbolCard);
+        this.cardPlace.appendChild(divCard);
     };
 
     shuffle(array) {
@@ -43,30 +50,59 @@ class Card {
         return array;
     };
 
+showNumberOfClics(){
+    this.placeTotalClics = document.querySelector('span');
+    this.placeTotalClics.innerHTML = this.numberOfTotalClick;
+}
+
+
+
 play() {
-    this.cardsSelected = document.querySelectorAll('.cardCont');
+    this.cardsSelected = document.querySelectorAll('.colCard');
 
     this.cardsSelected.forEach(cardSelected => {
         cardSelected.addEventListener('click', event => {
-            if (this.idSelectedCard !== event.target.id && event.target.className !== "cardSuccess" && this.numberOfClick < 2) {
-                this.returnCard(cardSelected);
 
+            //On retourne une carte si : 
+            // -la carte que l'on souhaite séléctionner n'est pas déja séléctionné (clique sur la même carte)
+            // -la carte que l'on souhaite séléctionner n'est pas déjà été remporté (en vert)
+            // -que l'on ne séléctionne pas plus de 2 cartes
+
+            if (this.card1Id!==event.target.id && this.dataSymbolCard !== event.target.dataset.symbol && event.target.className !== "col-lg-3 cardSuccess" && this.numberOfClick < 2) {
+                this.returnCard(cardSelected);
+                this.numberOfTotalClick++;
+                console.log(this.numberOfTotalClick)
             }
+
+            console.log('Le data symbole de la Carte 1  est :'+this.card1+'');
+            console.log('Le data symbole de la Carte 2  est :'+this.card2+'');
+
             if (this.card1 == this.card2) {
-                this.cardsWinner = document.getElementsByName(this.card1);
+                console.log('WINNN');
+                this.cardsWinner = document.querySelectorAll('.cardBack[data-symbol ="'+this.card1+'"]');
                 this.cardsWinner.forEach(cardWinner => {
-                    this.ifWin(cardWinner);
+                  this.ifWin(cardWinner);
+                  console.log(this.numberOfCorrect);
                 });
 
             }
-            this.idSelectedCard = event.target.id;
-            this.signCard = event.target.innerHTML;
 
-            if (event.target.className !== "cardSuccess") {
+              if (this.numberOfClick== 2 && event.target.className !== ".col-lg-3 cardSuccess") {
                 setTimeout(() => {
-                    this.ifWrong(cardSelected);
-                }, 1500);
-            }
+                    this.cardLoser1 = document.querySelector('.cardBack[data-symbol ="'+this.card1+'"]');
+                    this.cardLoser2 = document.querySelector('.cardBack[data-symbol ="'+this.card2+'"]');
+                    
+                    this.ifWrong(this.cardLoser1);
+                    this.ifWrong(this.cardLoser2);
+                }, 1000);
+            }  
+
+            
+             if (this.numberOfCorrect == this.unicodes.length){
+                this.winWindow(); 
+             }
+
+             this.showNumberOfClics();
         }
         );
     });
@@ -81,37 +117,44 @@ play() {
         }
 
     }
-    selectCard1(cardSelected) {
 
-        cardSelected.className = 'cardBack';
-        this.card1 = event.target.innerHTML;
+    selectCard1(cardSelected) {
+        cardSelected.className = "col-lg-3 cardBack";
+        this.card1 = event.target.dataset.symbol;
+        this.card1Id = event.target.id;
         this.numberOfClick = 1;
 
     };
-
     selectCard2(cardSelected) {
-        //selection carte 2
-        cardSelected.className = 'cardBack';
-        this.card2 = event.target.innerHTML;
+        cardSelected.className = "col-lg-3 cardBack";
+        this.card2 = event.target.dataset.symbol;
         this.numberOfClick = 2;
     }
 
 
     ifWin(cardWinner) {
-        cardWinner.className = "cardSuccess";
+        cardWinner.className = "col-lg-3 cardSuccess";
         this.numberOfClick = 0;
-        this.card1 = "";
-        this.card2 = "";
-        /* cardWinner.style.backgroundColor = "green";  */
+        this.card2 = undefined;
+        this.numberOfCorrect+=1;
+
     };
 
 
     ifWrong(cardSelected){
-        cardSelected.className = 'cardCont';
+        cardSelected.className = 'col-lg-3 colCard';
         this.numberOfClick = 0;
-        this.card1 = "";
-        this.card2 = "";
+        this.card1 = undefined;
+        this.card2 = undefined;
     }
+
+    winWindow(){
+        window.confirm('Vous avez gagné');
+        if (confirm ("Vous avez gagné")){
+            (location.reload(), false);
+        }
+    }
+
 
     reloadGame(){
         let refresh = document.querySelector('button');
@@ -133,6 +176,7 @@ play() {
 
 let game = new Card();
 game.play()
+game.showNumberOfClics();
 game.restart();
 
 
